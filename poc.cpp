c172e56f1dcbd2c0b0d4caeed84f13110589d192
Eduardo Costa <m4c0@users.noreply.github.com>
2024-11-28 18:01:07 +0000

    listing tests recursively

--- poc.cpp
+++ poc.cpp
@@ -12,11 +12,11 @@ import pprent;
 import print;
 import silog;
 
-bool run_test(jute::view dir) try {
-  using namespace jute::literals;
-  auto base_dir = "yaml-test-suite/"_s + dir + "/";
+bool run_test(auto dir) try {
+  auto in_yaml = (dir + "in.yaml").cstr();
+  if (mtime::of(in_yaml.begin()) == 0) silog::die("invalid test file: %s", in_yaml.begin());
 
-  auto in_json = (base_dir + "in.json").cstr();
+  auto in_json = (dir + "in.json").cstr();
   if (mtime::of(in_json.begin())) {
     auto json_src = jojo::read_cstr(in_json);
     auto view = jute::view { json_src };
@@ -27,7 +27,8 @@ bool run_test(jute::view dir) try {
     // TODO: deal with positive tests
     return false;
   }
-  auto err_file = (base_dir + "error").cstr();
+
+  auto err_file = (dir + "error").cstr();
   if (mtime::of(err_file.begin())) {
     // TODO: deal with negative tests
     return false;
@@ -36,13 +37,24 @@ bool run_test(jute::view dir) try {
   // TODO: how to test these? (example: M5DY)
   return false;
 } catch (...) {
-  silog::whilst("running test [%s]", dir.begin());
+  silog::whilst("running test [%s]", dir.cstr().begin());
+}
+
+void recurse(jute::view base) {
+  for (auto dir : pprent::list(base.begin())) {
+    if (dir[0] == '.') continue;
+    auto base_dir = base + jute::view::unsafe(dir) + "/";
+
+    if (mtime::of((base_dir + "/===").cstr().begin()) != 0) {
+      if (!run_test(base_dir)) silog::log(silog::error, "test failed: %s", base_dir.cstr().begin());
+    } else {
+      recurse(base_dir.cstr());
+    }
+  }
 }
 
 int main() {
-  for (auto dir : pprent::list("yaml-test-suite/name")) {
-    if (dir[0] == '.') continue;
-    if (!run_test(jute::view::unsafe(dir))) silog::log(silog::error, "test failed: %s", dir);
-  }
+  jute::view base = "yaml-test-suite/name/";
+  recurse(base);
 }
 
